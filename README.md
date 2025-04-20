76d0b27102448c45b2f4aef5eaf169330923e6ff
Matteo Redaelli <matteo.redaelli@gmail.com>
2025-04-20 16:12:56 +0200

    using typer instead of fire

--- README.md
+++ README.md
@@ -1,98 +1,73 @@
 # msAD
 
+*msad* is a library and command line tool for working with an Active Directory / LDAP server from Unix, Linux and MacOs systems.
 
-msad is a library and command line tool for working with an Active Directory / LDAP server. It can be used for:
+It supports authentication with user/pwd and kerberos
+
+It can be used for:
 - search objects (users, groups, computers,..)
 - search group members
 - add/remove members to/from AD groups using DN or sAMaccoutName
 - change AD passwords
 - check if a user is disabled or locked, group membership
 
+## Prerequisites
+
+python >= 3.9
+
+For kerboros auth
+
+  - krb5 lib and tools (like kinit, ...)
+  - a keytab file or 
+  
+## Installation
+
+```bash
+pipx install msad
+```
+
+## COnfiguration
+
+Create a configuration file in $HOME/.msad.toml as suggested by
+
+```bash
+msad get-sample-config
+```
+
 
 ## Usage
 
+
 ```bash
 msad --help
+
+python -m msad --help
+
 ```
 
+
+For kerberos authentication, first you need to login to AD / get a ticket kerberos with
+
+```bash
+kinit youraduser
+```
+
+
+
 ```text
- COMMAND is one of the following:
+msad search "(samaccountname=matteo)"  --out-format=json
 
-     add_member
-       Adds the user to a group (using DN or sAMAccountName)
-
-     change_password
-
-     check_user
-       Get some info about a user: is it locked? disabled? password expired?
-
-     group_flat_members
-       Extract all the (nested) members of a group
-
-     group_member
-       Check if the user is a member of a group (using DN or sAMAccountName)
-
-     group_members
-       Extract the direct members of a group
-
-     has_expired_password
-       Check is user has the expired password
-
-     has_never_expires_password
-       Check if a user has never expires password
-
-     is_disabled
-       Check if a user is disabled
-
-     is_locked
-       Check if the user is locked
-
-     remove_member
-       Remove the user from a group (using DN or sAMAccountName)
-
-     search
-
-     user_groups
-       Extract the list of groups of a user (using DN or sAMAccountName)
-
-     users
-       Find users inside AD. The filter can be the cn or userPrincipalName or samaccoutnname or mail to be searched. Can contain *
+msad search "(cn=redaelli*)" --attribute mail --attribute samaccountname --out-format=json
 
 ```
 
 ## Sample
 
-I find useful to add an alias in my ~/.bash_aliases
-
-```bash
-alias msad='/usr/local/bin/msad --host=dmc1it.group.redaelli.org --port=636 --search_base dc=group,dc=redaelli,dc=org'
-```
-
-Retreive info about a user
-
-```bash
-msad check_user matteo 90 \[qliksense_analyzer,qliksense_professional\] 2>/dev/null
-```
-
-```json
-{"is_disabled": false}
-{"is_locked": false}
-{"has_never_expires_password": false}
-{"has_expired_password": false}
-{"membership_qliksense_analyzer": false}
-{"membership_qliksense_professional": true}
-```
-
-Getting nested group members (it is a pages search, it can retreive more than 1000 users)
-
-```bash
-msad --out_format csv --attributes samaccountname,mail,sn,givenName group_flat_members "dc=group,dc=redaelli,dc=org" --group_name "qliksense_admin"
-```
 
 
 ## License
 
-Copyright © 2021 2022 Matteo Redaelli
+Copyright © 2021 - 2025 Matteo Redaelli
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
---
+++ src/msad/main.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+
+# msad - Active Directory tool
+# Copyright (C) 2025 - matteo.redaelli@gmail.com
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+import logging
+import datetime
+import os
+import json
+import ssl
+import sys
+import tomllib
+
+import typer
+
+import msad
+import ldap3
+
+from pathlib import Path
+
+
+BANNER = """
+
+"""
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logging.info(BANNER)
+    
+def _get_domain_config(config: dict|None, domain: str|None):
+    if "defaults" not in config:
+        logging.error(f"Missing entry 'defaults' in config file. Bye!")
+        sys.exit(100)
+    if "domain" not in config["defaults"]:
+        logging.error(f"Missing entry 'domain' in section 'defaults' in config file. Bye!")
+        sys.exit(101)
+        
+    default_domain = config["defaults"]["domain"]
+    
+    if "domains" not in config:
+        logging.error(f"Missing section 'domains' in config file. Bye!")
+        sys.exit(102)
+        
+    if default_domain not in config["domains"] :
+        logging.error(f"Missing section '{default_domain}' in section 'domains' in config file. Bye!")
+        sys.exit(103)
+
+    if domain and domain not in config["domains"] :
+        logging.error(f"Missing section '{domain}' in section 'domains' in config file. Bye!")
+        sys.exit(104)
+
+    # TODO: validateing domain sections
+    if not domain:
+        domain = default_domain
+
+    domain_config = config["domains"][domain]
+    for field in ["host", "port", "search_base", "use_ssl"]:
+        if field not in domain_config:
+            logging.error(f"Missing required field '{field}' in section 'domains.{domain}' in config file. Bye!")
+            sys.exit(105)
+    return domain_config
+        
+def _get_config(domain: str|None, config_file: str|None):
+    if not config_file:
+        home = Path.home()
+        config_file = home / ".msad.toml"
+
+    if not os.path.isfile(config_file):
+        logging.error(f"Missing file {config_file}. Bye!")
+        sys.exit(1)
+        
+    if not os.access(config_file, os.R_OK):
+        logging.error(f"File {config_file} is not readable. Bye!")
+        sys.exit(2)
+    
+    """Read config file"""
+    with open(config_file, "rb") as f:
+        data = tomllib.load(f)
+        return _get_domain_config(data, domain)
+
+def _json_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+    elif isinstance(o, list):
+        return ";".join(o)
+    # else return o
+
+
+def _get_connection_krb(host: str, port: int, use_ssl: bool):
+    tls = ldap3.Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1_2)
+    server = ldap3.Server(host, port=port, use_ssl=use_ssl, tls=tls)
+
+    conn = ldap3.Connection(
+        server,
+        authentication=ldap3.SASL,
+        sasl_mechanism=ldap3.KERBEROS,
+        auto_bind=False,
+    )
+    # conn.bind()
+    return conn
+
+
+def _get_connection_user_pwd(host: str, port: int, use_ssl: bool, user: str, password: str):
+    server = ldap3.Server(host, port=port, use_ssl=use_ssl)
+
+    conn = ldap3.Connection(server, user=user, password=password, auto_bind=False)
+    # conn.bind()
+    return conn
+
+
+def _get_connection(config: dict):
+    if "user" in config and "password" in config:
+        conn = _get_connection_user_pwd(config["host"],
+                                        config["port"],
+                                        config["use_ssl"],
+                                        config["user"],
+                                        config["password"])
+    else:
+        conn = _get_connection_krb(config["host"],
+                                   config["port"],
+                                   config["use_ssl"])
+    conn.bind()
+    return conn
+
+def _pprint(ldapresult, out_format, sep="\t"):
+    if not ldapresult or out_format == "default":
+        return ldapresult
+    elif out_format == "json1":
+        return json.dumps(dict(ldapresult))
+    else:
+        result = ""
+        for obj in ldapresult:
+            if out_format == "json":
+                result = (
+                    result + json.dumps(dict(obj), default=_json_converter) + "\n"
+                )
+            elif out_format == "csv":
+                sorted_obj = dict(sorted(obj.items()))
+                new_values = list(
+                    map(
+                        lambda v: "|".join(v) if isinstance(v, list) else v,
+                        sorted_obj.values(),
+                    )
+                )
+                result = result + sep.join(new_values) + "\n"
+        return result
+
+    # def users(self, user):
+    #     """Find users inside AD. The
+    #     filter can be the cn or userPrincipalName or samaccoutnname or mail to be searched. Can contain *
+    #     """
+    #     result = msad.users(
+    #         self._conn, self._search_base, user, attributes=self._attributes
+    #     )
+    #     return self._pprint(result)
+
+    # def is_disabled(self, user):
+    #     """Check if a user is disabled"""
+    #     return msad.user.is_disabled(self._conn, self._search_base, user)
+
+    # def is_locked(self, user):
+    #     """Check if the user is locked"""
+    #     return msad.user.is_locked(self._conn, self._search_base, user)
+
+    # def password_changed_in_days(self, user):
+    #     return msad.user.password_changed_in_days(self._conn, self._search_base, user)
+    
+    # def has_expired_password(self, user, max_age):
+    #     """Check is user has the expired password"""
+    #     return msad.has_expired_password(self._conn, self._search_base, user, max_age)
+
+    # def has_never_expires_password(self, user):
+    #     """Check if a user has never expires password"""
+    #     return msad.has_never_expires_password(self._conn, self._search_base, user)
+
+    # def check_user(self, user, max_age, groups=[]):
+    #     """Get some info about a user: is it locked? disabled? password expired?"""
+    #     return msad.check_user(self._conn, self._search_base, user, max_age, groups)
+
+    # def group_flat_members(self, group_name=None, group_dn=None):
+    #     """Extract all the (nested) members of a group"""
+    #     result = msad.group_flat_members(
+    #         self._conn,
+    #         self._search_base,
+    #         self._limit,
+    #         group_name,
+    #         group_dn,
+    #         attributes=self._attributes,
+    #     )
+    #     return self._pprint(result)
+
+    # def group_members(self, group_name=None, group_dn=None):
+    #     """Extract the direct members of a group"""
+    #     if group_name is None and group_dn is None:
+    #         logging.error("group_name or group_dn must be entered")
+    #         return None
+    #     result = msad.group_members(self._conn, self._search_base, group_name, group_dn)
+    #     return self._pprint(result)
+
+    # def add_member(self, group_name=None, group_dn=None, user_name=None, user_dn=None):
+    #     """Adds the user to a group (using DN or sAMAccountName)"""
+    #     return msad.add_member(
+    #         conn=self._conn,
+    #         search_base=self._search_base,
+    #         group_name=group_name,
+    #         group_dn=group_dn,
+    #         user_name=user_name,
+    #         user_dn=user_dn,
+    #     )
+
+    # def user_groups(self, user_name=None, user_dn=None):
+    #     """Extract the list of groups of a user (using DN or sAMAccountName)"""
+    #     return msad.user.user_groups(
+    #         self._conn, self._search_base, self._limit, user_name, user_dn
+    #     )
+
+    # def remove_member(
+    #     self, group_name=None, group_dn=None, user_name=None, user_dn=None
+    # ):
+    #     """Remove the user from a group (using DN or sAMAccountName)"""
+    #     return msad.remove_member(
+    #         conn=self._conn,
+    #         search_base=self._search_base,
+    #         group_name=group_name,
+    #         group_dn=group_dn,
+    #         user_name=user_name,
+    #         user_dn=user_dn,
+    #     )
+
+    # def group_member(
+    #     self, group_name=None, group_dn=None, user_name=None, user_dn=None
+    # ):
+    #     """Check if the user is a member of a group (using DN or sAMAccountName)"""
+    #     return msad.group_member(
+    #         conn=self._conn,
+    #         search_base=self._search_base,
+    #         group_name=group_name,
+    #         group_dn=group_dn,
+    #         user_name=user_name,
+    #         user_dn=user_dn,
+    #     )
+
+
+
+app = typer.Typer()
+
+@app.command()
+def change_password(userid: str|None, userdn: str|None, domain: str|None, config_file: str|None):
+    config = _get_config(config_file, domain)
+    conn = _get_connection(config)
+    return msad.user.change_password(
+            conn, config["search_base"], userid, userdn)
+
+@app.command()
+def search(filter: str, limit: int = 2000, domain: str|None = None, config_file: str|None = None, out_format: str = "json", attributes: list[str] = []):
+    config = _get_config(config_file, domain)
+    conn = _get_connection(config)
+    result = msad.search(conn, config["search_base"], filter, limit=limit, attributes=attributes)
+    print(_pprint(result, out_format))
+
+@app.command()
+def get_sample_config():
+    output = """
+[defaults]
+
+domain = "mydomain"
+
+[domains]
+
+[domains.mydomain]
+
+host = "example.com"
+search_base = "dc=example,dc=com"
+    
+port = 636
+use_ssl = true
+#port = 389
+#use_ssl = false
+
+# user =
+# password =
+"""
+    print(output)
+    
+if __name__ == "__main__":
+    app()
